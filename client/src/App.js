9b515840335023066bf66b6692a6b7395400cee0
Matt Hale <mhale@factharbor.com>
2021-05-25 11:23:55 -0500

    removed app css formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Search from "./components/search/index";
-import Saved from "./components/saved/index";
+// import Search from "./components/search/index";
+// import Saved from "./components/saved/index";
 import "./App.css";
 
 function App() {
@@ -12,10 +12,10 @@ function App() {
           <h3><a href="/">Search</a></h3>
           <h3><a href="/saved">Saved</a></h3>
         </nav>
-        <Switch>
+        {/* <Switch>
           <Route exact path="/" component={Search} />
           <Route exact path="/saved" component={Saved} />
-        </Switch>
+        </Switch> */}
       </div>
     </Router>
   );
