5734cf43ee6c4f7f8875091fb8942bba120a3b04
Jules Scheil <juliana.m.scheil@gmail.com>
2021-05-26 09:22:05 -0400

    adding associations

--- models/index.js
+++ models/index.js
@@ -1,8 +1,44 @@
+const User= require('./users');
+const ForSalePost= require('./forSalePost');
+const ForSaleComment= require('./forSaleComment');
+const ForumPost= require('./forumPost');
+const ForumComment= require('./forumComment');
+
+ForSalePost.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+ForSalePost.hasMany(ForSaleComment, {
+    foreignKey: 'sale_[pst_id',
+    onDelete: 'CASCADE'
+})
+
+ForSaleComment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+ForumPost.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+ForumPost.hasMany(ForumComment, {
+    foreignKey: 'sale_[pst_id',
+    onDelete: 'CASCADE'
+})
+
+ForumComment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
 module.exports = {
-    User: require('./users'),
-    ForSalePost: require('./forSalePost'),
-    ForSaleComment: require('./forSaleComment'),
-    ForumPost: require('./forumPost'),
-    ForumComment: require('./forumComment')
+    User,
+    ForSalePost,
+    ForSaleComment,
+    ForumPost,
+    ForumComment
 };
   
