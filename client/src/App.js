cdb1560a5b9e69dc474b90d1102bb7bead894af2
joydotcom <msjoyjackson@gmail.com>
2021-05-26 11:23:30 -0400

    added styling for login out, tranferred code to app js for logout button in nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SaleCard from "./components/ForSaleCard/index";
 import ForumCard from "./components/ForumCard/index";
@@ -8,9 +8,27 @@ import Sale from "./Pages/forSale/index";
 import forumPosts from "./Pages/forumPosts/index";
 import Home from "./Pages/Login/index";
 import NoMatch from "./Pages/NoMatch/index"
+import API from "./utils/API";
 import "./App.css";
 
+
+
 function App() {
+
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+const handleLogout = (event) => {
+  event.preventDefault();
+  API.userLogout()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  setName("");
+  setEmail("");
+  setPassword("");
+};
+
   return (
     <Router>
       <Navbar bg="light" variant="light">
@@ -20,12 +38,14 @@ function App() {
       <Nav.Link href="/forum">Forum</Nav.Link>
       <Nav.Link href="/favorites">favorites</Nav.Link>
       <Nav.Link href="/forsale">For Sale</Nav.Link>
-      <Nav.Link href="#pricing">Logout</Nav.Link>
-    </Nav>
+      <div class="btn btn-outline-secondary">
+      <Button  onClick={handleLogout}>Logout</Button>
+    </div>
     {/* <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-primary">Search</Button>
     </Form> */}
+    </Nav>
   </Navbar>
       <div className="App">
         <Switch>
@@ -34,6 +54,7 @@ function App() {
           <Route exact path="/forum" component={forumPosts} />
           <Route exact path="/favorites" component={Favorites} />
           <Route  component={NoMatch} />
+         
         </Switch>
       </div>
     </Router>
@@ -41,3 +62,4 @@ function App() {
 }
 
 export default App;
+
