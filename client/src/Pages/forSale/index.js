c3ba001b625d1c517996871dc5ca86662944296a
Jules Scheil <juliana.m.scheil@gmail.com>
2021-05-27 09:38:50 -0400

    added for sale new post card

--- client/src/Pages/forSale/index.js
+++ client/src/Pages/forSale/index.js
@@ -1,18 +1,9 @@
 import React, {useState} from "react";
-// import { Card, Button } from "react-bootstrap";
-import { Container, Row, Col, Button, DropdownButton } from "react-bootstrap";
-// import API from "../../utils/API";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "./style.css";
 import ForSaleCard from '../../components/ForSaleCard/index'
-import Dropdown from 'react-bootstrap/Dropdown'
-
+import ForSalePostCard from '../../components/ForSalePostCard/index';
 const Sale = () => {
-  
-  const [category,setCategory]=useState('');
-  const handleSelect=(e)=>{
-    console.log(e);
-    setCategory(e)
-  };
 
   return (
     <Container>
@@ -24,32 +15,9 @@ const Sale = () => {
           <div className="card-header">
             <h2>Create New Post</h2>
           </div>
-          <form id="new-post-form" className="card-body">
-            <label className="form-label" for="post-title">Title</label>
-            <input type="text" name="post-title" className="form-input" />
-
-            <label className="form-label" for="post-body">Content</label>
-            <textarea name="post-body" className="form-input"></textarea>
-
-            <div class="dropdown">
-              <DropdownButton 
-              alignRight
-              title="Category"
-              id="dropdown-menu-align-right"
-              onSelect={handleSelect}
-              >
-                <Dropdown.Item eventKey="Indoor">Indoor</Dropdown.Item>
-                <Dropdown.Item eventKey="Outdoor">Outdoor</Dropdown.Item>
-                <Dropdown.Item eventKey="Her">Herb</Dropdown.Item>
-                <Dropdown.Item eventKey="Veggie">Veggie</Dropdown.Item>
-                </DropdownButton>
-            </div>
-
-            <label className="form-label" for="post-title">Location</label>
-            <input type="text" name="post-title" className="form-input" />
-            <Button type="submit" className="btn">Create</Button>
-          </form>
+       <ForSalePostCard />
         </div>
+
       </div> 
     </Container>
   );
