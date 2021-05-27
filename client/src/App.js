cc559718180a65f0921947a4b3bae94455cfa4f1
Matt Hale <mhale@factharbor.com>
2021-05-27 10:19:09 -0500

    moved login and signup forms to the APP.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SaleCard from "./components/ForSaleCard/index";
 import ForumCard from "./components/ForumCard/index";
-import { Nav, Form, Navbar, FormControl, Button } from "react-bootstrap";
+import {
+  Nav,
+  Form,
+  Navbar,
+  FormControl,
+  Button,
+  Container,
+} from "react-bootstrap";
 import Favorites from "./Pages/Favorites/index";
 import Sale from "./Pages/forSale/index";
 import forumPosts from "./Pages/forumPosts/index";
@@ -11,21 +18,37 @@ import NoMatch from "./Pages/NoMatch/index";
 import API from "./utils/API";
 import "./App.css";
 
-
-
 function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+    API.userSignup({ name, newEmail, newPassword })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    setName("");
+    setNewEmail("");
+    setNewPassword("");
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    API.userLogin({ email, password })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    setEmail("");
+    setPassword("");
+  };
 
   const handleLogout = (event) => {
     event.preventDefault();
     API.userLogout()
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
-    setName("");
-    setEmail("");
-    setPassword("");
   };
 
   return (
@@ -48,7 +71,64 @@ function App() {
       </Navbar>
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/">
+            <Container>
+              <div className="form-group">
+                <h1>Signup Form</h1>
+                <form onSubmit={handleSignup}>
+                  <input
+                    type="name"
+                    className="form-control"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                      value={newEmail}
+                      onChange={(e) => setNewEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control"
+                      type="password"
+                      placeholder="Password"
+                      value={newPassword}
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                  </div>
+                  <button type="submit">Signup</button>
+                </form>
+                <h1>Login Form</h1>
+                <div className="form-group">
+                  <form onSubmit={handleLogin}>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <input
+                      type="password"
+                      className="form-control"
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="submit">Login</button>
+                  </form>
+                </div>
+              </div>
+            </Container>
+          </Route>
           <Route exact path="/forsale" component={Sale} />
           <Route exact path="/forum" component={forumPosts} />
           <Route exact path="/favorites" component={Favorites} />
