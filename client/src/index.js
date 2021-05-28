30056f6cdcf82d3797152a20919518263b654ee9
Matt Hale <mhale@factharbor.com>
2021-05-28 08:04:03 -0500

    added rerouting after login and improved session user route

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import registerServiceWorker from "./registerServiceWorker";
-import 'bootstrap';
-import 'bootstrap/dist/js/bootstrap.js';
+import "bootstrap";
+import "bootstrap/dist/js/bootstrap.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
