911cd8ee71113b66e4abdbc01d251d6a26db48f3
Matt Hale <mhale@factharbor.com>
2021-05-31 17:42:50 -0500

    added refresh to new posts and forum

--- client/src/components/ForumPostCard/index.js
+++ client/src/components/ForumPostCard/index.js
@@ -26,6 +26,7 @@ const NewPost = (props) => {
         console.log(res);
         setTitle("");
         setDescription("");
+        window.location.reload();
       })
       .catch((err) => console.log(err));
   };
